4300657577449ecc4487de272caf30ee7e6e343f
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2021-06-27 14:35:12 -0400

    Use masterfile/assets (#16)
    - Added manual files for easier updating of manual fields
    - Update.js script for grabbing official assets from pokeminer repos
    - Adds RU support

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Central repository of Pokemon translations for various projects
 
+- Submit manual updates to `/static/manual/`
+- To fetch latest Pokemon/Moves/Items, run `node update.js`
+
 ## Credits
 - [versx](https://github.com/versx)  
 - [PMSF](https://github.com/pmsf/PMSF)  
---
+++ update.js
@@ -0,0 +1,89 @@
+const fs = require('fs')
+const Fetch = require('node-fetch')
+const path = require('path')
+const POGOProtos = require('pogo-protos')
+
+function fetchJson(url) {
+  return new Promise(resolve => {
+    Fetch(url)
+      .then(res => res.json())
+      .then(json => resolve(json.data))
+  })
+}
+
+((async function createLocales() {
+  const available = {
+    de: 'german',
+    en: 'english',
+    es: 'spanish',
+    fr: 'french',
+    it: 'italian',
+    jp: 'japanese',
+    ko: 'korean',
+    'pt-br': 'brazilianportuguese',
+    th: 'thai',
+    'zh-tw': 'chinesetraditional',
+    ru: 'russian',
+  }
+
+  const remoteFiles = {}
+  await Promise.all(Object.entries(available).map(async language => {
+    const [key, value] = language
+    remoteFiles[key] = await fetchJson(`https://raw.githubusercontent.com/PokeMiners/pogo_assets/master/Texts/Latest%20APK/JSON/i18n_${value}.json`)
+  }))
+
+  const pogoLocalesFolder = path.resolve(__dirname, './static/manual')
+  fs.readdir(pogoLocalesFolder, (err, files) => {
+    files.forEach(file => {
+      const short = path.basename(file, '.json')
+      const safe = remoteFiles[short] ? short : 'en'
+      const nativeJson = {}
+
+      for (let i = 0; i < remoteFiles[safe].length; i += 2) {
+        nativeJson[remoteFiles[safe][i]] = remoteFiles[safe][i + 1]
+      }
+      const pokemon = {}
+      Object.values(POGOProtos.Rpc.HoloPokemonId).forEach(id => {
+        const key = `pokemon_name_${String(id).padStart(4, '0')}`
+        if (nativeJson[key]) {
+          if (id) {
+            pokemon[`poke_${id}`] = nativeJson[key]
+          }
+        }
+      })
+      const moves = {}
+      Object.values(POGOProtos.Rpc.HoloPokemonMove).forEach(id => {
+        const key = `move_name_${String(id).padStart(4, '0')}`
+        if (nativeJson[key]) {
+          moves[`move_${id}`] = nativeJson[key]
+        }
+      })
+      const items = {}
+      Object.entries(POGOProtos.Rpc.Item).forEach(id => {
+        const [key, value] = id
+        if (nativeJson[`${key.toLowerCase()}_name`]) {
+          items[`item_${value}`] = nativeJson[`${key.toLowerCase()}_name`]
+        }
+      })
+
+      const pogoTranslations = fs.readFileSync(
+        path.resolve(pogoLocalesFolder, file),
+        { encoding: 'utf8', flag: 'r' },
+      )
+      const manualKeys = JSON.parse(pogoTranslations.toString())
+
+      const final = {
+        ...pokemon,
+        ...moves,
+        ...items,
+        ...manualKeys,
+      }
+      fs.writeFile(
+        path.resolve(path.resolve(__dirname, './static/locales'), file),
+        JSON.stringify(final, null, 2),
+        'utf8',
+        () => { },
+      )
+    })
+  })
+})())
